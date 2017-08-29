46d2000826e6545fbe134f847e7dd00f69c532ae
Yue Pik Kong <yuepikkong@Yues-iMac.local>
2017-08-29 14:15:38 +0800

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Yue Pik Kong on 29/8/17.
+//  Copyright © 2017 Yue Pik Kong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
