3d8a776c884740bca903306e1c24e7f32bfd9fd7
Yue Pik Kong <yuepikkong@gmail.com>
2017-08-29 16:00:43 +0800

    My first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    var tapCount = 0
+    @IBAction func buttonTapped(_ sender: Any) {
+        theLabel.text = "Wow"
+        tapCount = tapCount + 1
+        print(tapCount)
+        if tapCount >= 10 {
+            theLabel.text = "You tapped the button 10 times"
+        }
+    }
+    
+    @IBAction func Return(_ sender: Any) {
+        theLabel.text = "Heelo World"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.red
+        theLabel.text = "Hello There!"
     }
 
     override func didReceiveMemoryWarning() {
